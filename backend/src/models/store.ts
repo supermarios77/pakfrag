13ece3038b1b77b0933e0262590a56bbe5c07492
Mariya B <supermariyabros@gmail.com>
2023-12-08 15:44:52 +0300

    Add Product Entity 🚘

--- backend/src/models/store.ts
+++ backend/src/models/store.ts
@@ -1,11 +1,14 @@
-import { Entity, OneToMany } from "typeorm"
+import { Entity, OneToMany } from "typeorm";
 import { User } from "./user";
+import { Product } from "./product";
 import {
   Store as MedusaStore,
-} from "@medusajs/medusa"
+} from "@medusajs/medusa";
 
 @Entity()
 export class Store extends MedusaStore {
     @OneToMany(() => User, (user) => user?.store)
   members?: User[];
+  @OneToMany(() => Product, (product) => product?.store)
+  products?: Product[];
 }
